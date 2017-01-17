41249edc2b524ea62c74687983f2d27947a41d31
Jayjader <njay47@gmail.com>
2017-01-17 15:42:29 +0100

    Account session manager w/ beans

--- src/accounts/model/Accounts.java
+++ src/accounts/model/Accounts.java
@@ -1,6 +1,7 @@
 package accounts.model;
 
 import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
 import javax.xml.bind.DatatypeConverter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -14,55 +15,60 @@ import java.util.Map;
 @Singleton
 public class Accounts implements IAccountsLocal, IAccountsRemote {
 
-  private Map<String, String[]> accountsList;
-  private MessageDigest sha256;
+    @PersistanceManager
+    EntityManager em;
 
-  public Accounts() {
-    this.accountsList = new HashMap<>();
-    try {
-      this.sha256 = MessageDigest.getInstance("SHA-256");
-    } catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-    }
-  }
+    private Map<String, String[]> accountsList;
+    private MessageDigest sha256;
 
-  @Override
-  public boolean addAccount(String username, String email, String password) {
-    if (!username.equals("") && !exists(username)) {
-      this.accountsList.put(username, new String[] {email,
-              DatatypeConverter.printHexBinary(sha256.digest(password.getBytes())).toLowerCase()});
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  @Override
-  public boolean exists(String username) {
-    return this.accountsList.containsKey(username);
-  }
-
-  @Override
-  public String connection(String username, String password) {
-    String[] found = this.accountsList.get(username);
-    String key;
-    if (found != null
-            && found[1].equals(DatatypeConverter.printHexBinary(sha256.digest(password.getBytes())).toLowerCase())) {
-      key = username + (Calendar.getInstance().getTimeInMillis() / 100000) + "ok";
-    } else {
-      key = "nok";
+    public Accounts() {
+        try {
+            this.sha256 = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 
-    return DatatypeConverter.printHexBinary(sha256.digest(key.getBytes())).toLowerCase();
-  }
-
-  @Override
-  public String getEmail(String username) {
-    String[] found = this.accountsList.get(username);
-    if (found != null) {
-      return found[0];
-    } else {
-      return "";
+    @Override
+    public boolean addAccount(String username, String email, String password) {
+        if (!username.equals("") && !exists(username)) {
+            UsersEntity u = new UsersEntity();
+            u.setUsername(username);
+            u.setPassword(password);
+            u.setEmail(email);
+            em.persist(u);
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
+
+    @Override
+    public boolean exists(String username) {
+        return (this.em.find(UsersEntity.class, username) != null);
+    }
+
+    @Override
+    public String connection(String username, String password) {
+        UserEntity user = this.em.find(UsersEntity.class, username);
+        String key;
+        if (user != null
+                && user.getPassword().equals(DatatypeConverter.printHexBinary(sha256.digest(password.getBytes())).toLowerCase())) {
+            key = username + (Calendar.getInstance().getTimeInMillis() / 100000) + "ok";
+        } else {
+            key = "nok";
+        }
+
+        return DatatypeConverter.printHexBinary(sha256.digest(key.getBytes())).toLowerCase();
+    }
+
+    @Override
+    public String getEmail(String username) {
+        UserEntity user = this.em.find(UsersEntity.class, username);
+        if (user != null) {
+            return user.getEmail();
+        } else {
+            return "";
+        }
+    }
 }
